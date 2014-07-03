c764e92cd44228bb00f125cded05bd90e8ffb5be
marv01 <ffoeg01@gmail.com>
2014-07-02 20:52:51 -0700

    day 1-4

---
+++ app/states.js
@@ -0,0 +1,42 @@
+(function(angular){
+	"use strict";
+	var app = angular.module('Swagwise');
+	app.config(function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider
+			.otherwise('/');
+		$stateProvider
+			.state('home',{
+				url:'/',
+				templateUrl:'views/home.html'
+			})
+			.state('about',{
+				url:'/about',
+				templateUrl:'views/about.html'
+			})
+			.state('cart',{
+				url:'/cart',
+				templateUrl:'views/cart.html'
+			})
+			.state('contact',{
+				url:'/contact',
+				templateUrl:'views/contact.html'
+			})
+			.state('detail',{
+				url:'/detail',
+				templateUrl:'views/detail.html'
+			})
+			.state('login',{
+				url:'/login',
+				templateUrl:'views/login.html'
+			})
+			.state('signup',{
+				url:'/signup',
+				templateUrl:'views/signup.html'
+			})
+			.state('swag',{
+				url:'/swag',
+				controller:'SwagController',
+				templateUrl:'views/swag.html'
+			});
+		});
+})(window.angular);
